ad33639a70ef6ab1c4a4ec4b76a227aee83623f2
Infer-Noam <noam2468noam@gmail.com>
2024-03-28 17:41:29 +0200

    git hub upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.Infernoam.musicalchairs"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.Infernoam.musichairs"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 7
+        versionName = "3.1"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.preference:preference:1.2.0")
+    implementation("com.google.android.gms:play-services-ads:22.5.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.facebook.android:facebook-android-sdk:[8,9)")
+}
+
